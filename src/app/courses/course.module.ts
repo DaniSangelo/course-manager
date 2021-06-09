15b053a8e9c9296c143f99a4d819519863f4838f
Daniel Sângelo Passos <dpassos@nelogica.com.br>
2021-06-09 20:23:33 -0300

    Criando a pasta shared e movendo o componente star e o pipe replace para dentro dela

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -3,20 +3,20 @@ import { RouterModule } from '@angular/router';
 import { CourseInfoComponent } from './course-info.component';
 import { CourseListComponent } from './course-list.component';
 import { FormsModule } from '@angular/forms'
-import { ReplacePipe } from '../pipe/replace.pipe';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from '../star/star.component';
+import { StarModule } from '../shared/component/star/star.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 
 @NgModule({
   declarations:[
     CourseInfoComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe
+    CourseListComponent
   ],
   imports: [
     FormsModule,
     CommonModule,
+    StarModule,
+    AppPipeModule,
     RouterModule.forChild([
       {
         path:'courses/info/:id', component: CourseInfoComponent
